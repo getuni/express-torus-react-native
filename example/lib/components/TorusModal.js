2c443c7b22b2d5d7c9a36949ee1642d9b77eb385
Alex Thomas <cawfree@Alexs-MacBook-Pro.local>
2020-09-13 06:38:35 +0100

    chore: verify working webview implementation

--- example/lib/components/TorusModal.js
+++ example/lib/components/TorusModal.js
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState, useRef, useEffect } from "react";
 import PropTypes from "prop-types";
 import ModalBox from "react-native-modalbox";
 import { StyleSheet } from "react-native";
@@ -7,6 +7,12 @@ import { typeCheck } from "type-check";
 import { WebView } from ".";
 
 function TorusModal({ visible, source, onDismiss, onAuthResult }) {
+  const ref = useRef();
+  const [verify, setVerify] = useState(null);
+  const [trigger, setTrigger] = useState(null);
+  const [data, setData] = useState(null);
+  const [key, setKey] = useState(Math.random);
+
   const onMessage = useCallback(
     ({ nativeEvent: { data } }) => {
       console.warn(data);
@@ -19,20 +25,58 @@ function TorusModal({ visible, source, onDismiss, onAuthResult }) {
             return onAuthResult(data);
           } else if (type === "torus-verifier-result") {
             const { result } = extras;
-            return console.warn('got verifier', result);
+            return setVerify(result);
+          } else if (type === "torus-trigger-auth") {
+            const { trigger } = extras;
+            return setTrigger(trigger);
+          //} else if (type === "torus-recovered") { // TODO: This should be torus-auth!
+          //  const { result } = extras;
+          //  return onAuthResult(result, false);
           }
           throw new Error(`Unexpected type, ${type}.`);
         }
       } catch (e) {}
     },
-    [onAuthResult],
+    [onAuthResult, setVerify, ref, setTrigger],
+  );
+  useEffect(
+    () => {
+      if (!!trigger && !!verify) {
+        const { ...data } = { ...trigger, ...verify };
+        setVerify(null);
+        setTrigger(null);
+        setData(data);
+        setKey(Math.random);
+      }
+    },
+    [trigger, verify, ref, setVerify, setTrigger, setData, setKey],
+  );
+  const onLoadEnd = useCallback(
+    () => {
+      if (!!data) {
+        ref.current.injectJavaScript(
+          `
+setTimeout(
+  () => window.__REACT_TORUS_TRIGGER_VERIFY__(${JSON.stringify(data)}),
+  2000, // TODO: This needs to be dynamic.
+);
+true;
+          `.trim(),
+        );
+      }
+    },
+    [data, ref],
   );
   return (
     <ModalBox style={StyleSheet.absoluteFill} isOpen={visible} onClosed={onDismiss}>
       <WebView
+        key={key}
+        ref={ref}
         style={StyleSheet.absoluteFill}
+        onLoadEnd={onLoadEnd}
         source={source}
         onMessage={onMessage}
+        userAgent="Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20100101 Firefox/10.0"
       />
     </ModalBox>
   );
